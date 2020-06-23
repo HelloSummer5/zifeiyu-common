1551313cba77ed571f51f836c9ca6808270a610e
liuxing321 <asxingxinglg@163.com>
2020-06-23 18:27:48 +0800

    tj

--- src/main/java/com/zifeiyu/common/controller/TestSwaggerController.java
+++ src/main/java/com/zifeiyu/common/controller/TestSwaggerController.java
@@ -25,7 +25,6 @@ public class TestSwaggerController {
 
     @GetMapping("list")
     public String testMapper(){
-        String a= "";
         return classifyService.listPages().toString();
     }
 }
